8f02b733f9c7307b0a22bec8ece40a50fbdcee3c
Johnnie Hicks <johnnie@Johnnies-MacBook-Pro.local>
2021-08-23 20:07:36 -0400

    updated profile page

--- src/Mobile/Component/HomePage/HomePage.js
+++ src/Mobile/Component/HomePage/HomePage.js
@@ -4,8 +4,14 @@ function HomePage(){
     return(
         <div className="homepage-main">
             <div className='homepage__header'>
-            <h1>Johnnie Hick</h1>
-            <h4>FullStack Engineer</h4>
+                <h1>Johnnie Hick</h1>
+                <h4>FullStack Engineer</h4>
+            </div>
+            <div className='image'>
+                <div className='homepage__image'></div>
+            </div>
+            <div className='homepage__content'>
+                <p>Hey, My name is Johnnie! Lorem IspumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrudr</p>
             </div>
         </div>
     )
