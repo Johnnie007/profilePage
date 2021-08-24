52a0943507e43034e19df71427ea55c66e9e850f
Johnnie Hicks <johnnie@Johnnies-MBP.lan1>
2021-08-24 14:34:14 -0400

    updated footer

--- src/Mobile/Component/Footer/Footer.js
+++ src/Mobile/Component/Footer/Footer.js
@@ -7,10 +7,10 @@ function Footer(){
     return(
         <div className="footer-main">
             <div className='footer-icon'>
-                <a href="mailto:Johnnie.l.hick8@gmail.com" ><img className='github-icon' src={GitHub} alt='github icon'/></a>
+                <a href="https://github.com/Johnnie007" ><img className='github-icon' src={GitHub} alt='github icon'/></a>
             </div>
             <div className='footer-icon'>
-                <a href="https://github.com/Johnnie007"><img className='gmail-icon' src={Gmail} alt='gmail icon'/></a>
+                <a href="mailto:Johnnie.l.hick8@gmail.com"><img className='gmail-icon' src={Gmail} alt='gmail icon'/></a>
             </div>
             <div className='footer-icon'>
                <a href="https://www.linkedin.com/in/johnnie-hicks-a3990320a/" ><img className='linkedin-icon' src={LinkedIn} alt='linkedin icon'/></a>
