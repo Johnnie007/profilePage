600edd1bed898c104ed8a00b13784611ae9094bb
Johnnie Hicks <johnnie@Johnnies-MBP.lan1>
2021-08-24 14:31:15 -0400

    created footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Nav from "./Mobile/Component/Nav/Nav";
 import Skills from "./Mobile/Component/Skills/Skills";
 import HomePage from "./Mobile/Component/HomePage/HomePage";
 import Portfolio from "./Mobile/Component/Porfolio/Portfolio";
+import Footer from "./Mobile/Component/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-      {/* <HomePage/> */}
-      {/* <Skills/> */}
+      <HomePage/>
+      <Skills/>
       <Portfolio/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Mobile/Component/Footer/Footer.js
@@ -0,0 +1,22 @@
+import './Footer.scss'
+import GitHub from '../../../Images/GitHub.png';
+import Gmail from '../../../Images/gmail.png';
+import LinkedIn from '../../../Images/linkedIn.png'
+
+function Footer(){
+    return(
+        <div className="footer-main">
+            <div className='footer-icon'>
+                <a href="mailto:Johnnie.l.hick8@gmail.com" ><img className='github-icon' src={GitHub} alt='github icon'/></a>
+            </div>
+            <div className='footer-icon'>
+                <a href="https://github.com/Johnnie007"><img className='gmail-icon' src={Gmail} alt='gmail icon'/></a>
+            </div>
+            <div className='footer-icon'>
+               <a href="https://www.linkedin.com/in/johnnie-hicks-a3990320a/" ><img className='linkedin-icon' src={LinkedIn} alt='linkedin icon'/></a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
