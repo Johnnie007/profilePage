3a00a9e583e999439dc16b036449bb059196c3d5
Johnnie Hicks <johnnie@Johnnies-MacBook-Pro.local>
2021-08-24 22:43:36 -0400

    created skills component

---
+++ src/Desktop/Skills/Skills.js
@@ -0,0 +1,36 @@
+import './Skills.scss';
+
+function Skills(){
+
+    return(
+        <div className='desktop-skills-main'>
+            <div>
+                <h1>Skills</h1>
+            </div>
+            <div className='desktop-skills-area'>
+                <ul className='desktop-skills__ul'>
+                    <li className='desktop-skills__li'>JavaScript</li>
+                    <li className='desktop-skills__li'>Node.Js</li>
+                    <li className='desktop-skills__li'>Express.Js</li>
+                </ul>
+                <ul className='desktop-skills__ul'>
+                    <li className='desktop-skills__li'>React.Js</li>
+                    <li className='desktop-skills__li'>HTML</li>
+                    <li className='desktop-skills__li'>CSS</li>
+                </ul>
+                <ul className='desktop-skills__ul'>
+                    <li className='desktop-skills__li'>MySQL</li>
+                    <li className='desktop-skills__li'>APIs</li>
+                    <li className='desktop-skills__li'>DOM</li>
+                </ul>
+                <ul className='desktop-skills__ul'>
+                    <li className='desktop-skills__li'>GitHub</li>
+                    <li className='desktop-skills__li'>Heroku</li>
+                    <li className='desktop-skills__li'>OAuth</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
