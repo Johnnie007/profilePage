f3cf36aef4a9a6f3c38da4aa4b1d220adb7940ab
Johnnie007 <81275727+Johnnie007@users.noreply.github.com>
2022-03-11 08:59:18 -0500

    updated files

--- src/Desktop/DesktopHomePage/DesktopHomePage.js
+++ src/Desktop/DesktopHomePage/DesktopHomePage.js
@@ -1,17 +1,20 @@
 import './DesktopHomePage.scss'
 import DesktopSkills from '../DesktopSkills/DesktopSkills'
 
+//style={{backgroundImage: `url(${props.img})`}}
 
 function DesktopHomePage(props){
     console.log(props)
     return(
         <div className='desktop-homepage-main'>
             <div className='desktop-homepage__header'>
-                <div className='desktop-image' style={{backgroundImage: `url(${props.img})`}}></div>{props.bio ?<div className='desktop-homepage__content'>
+                <div className='desktop-image' ></div>
+                {/* {props.bio ?<div className='desktop-homepage__content'>
                     <p>{props.bio}</p>
-                </div> : <div className='desktop-homepage__content'>
+                </div> :  */}
+                <div className='desktop-homepage__content'>
                     <p>Hey! My name is Johnnie, and thanks for checking out my section on the internet. I am a Full Stack Developer from Florida. I love to build things with my hands and solve problems. I have a 6-year-old boxer named Georgia and a 2-year-old son named Jackson. During the winter, I like to hike, read books, and travel. The rest of the year, I am chilling or playing chess! Feel Free to contact me at any time!</p>
-                </div> }
+                </div> 
             </div>
             <DesktopSkills className='Skills'/>
         </div>
