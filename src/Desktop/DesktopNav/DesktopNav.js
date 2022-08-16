4eed7a11056b334e3c4f0c2403d2fe7eb8d0a40f
Johnnie432 <johnnie432@icloud.com>
2022-08-15 22:01:47 -0400

    updated the styling

--- src/Desktop/DesktopNav/DesktopNav.js
+++ src/Desktop/DesktopNav/DesktopNav.js
@@ -7,8 +7,8 @@ function DesktopNav(){
     <aside>
         <div className='desktop-nav-main'>
             <ul className='desktop-nav-ul'>
-            <NavLink className='text-color' activeClassName='text' to='/' exact><li className='desktop-nav-li'><h1>Home.</h1></li></NavLink>
-            <NavLink className='text-color' activeClassName='text' to='/portfolio'><li className='desktop-nav-li'><h1>Portfolio.</h1></li></NavLink>
+            <NavLink className='nav-link' activeClassName='nav-link__active' to='/' exact><li className='desktop-nav-li'><h1>Home.</h1></li></NavLink>
+            <NavLink className='nav-link' activeClassName='nav-link__active' to='/portfolio'><li className='desktop-nav-li'><h1>Portfolio.</h1></li></NavLink>
                 <li className='desktop-nav-li'><a  className='text-color' target="_blank" href='https://www.linkedin.com/in/johnnie-hicks-a3990320a/'><h1>LinkedIn.</h1></a></li>
                 <li className='desktop-nav-li'><a className='text-color' target="_blank" href='https://github.com/Johnnie007'><h1>GitHub.</h1></a></li>
                 <li className='desktop-nav-li desktop-nav-li__sm'><a className='text-color' href={resume} download='JohnnieResume'><h1>Download Resume.</h1></a></li>
